0e7b25dcc1663163f2f4bed7de864bd5e957cef3
Henry Christoffer <henrycmeen@me.com>
2025-02-20 13:06:44 +0100

    Oppdatert cli.py med nye funksjoner

--- dimo/cli.py
+++ dimo/cli.py
@@ -1,22 +1,32 @@
 import argparse
+import subprocess
+import sys
 from dimo.update_mets import update_dias_mets
 from dimo.test import run_test
 from dimo.report import generate_report
 
-def update_mets_command(args):
-    update_dias_mets(args.mets_file, args.content_dir, dry_run=args.dry_run)
-
-def test_command(args):
-    run_test(standard=args.standard, path=args.path)
-
-def report_command(args):
-    generate_report(path=args.path, format=args.format)
+def update_command(args):
+    """Update DIMO to the latest version"""
+    print("Updating DIMO to the latest version...")
+    try:
+        subprocess.check_call([
+            sys.executable, "-m", "pip", "install", "--upgrade",
+            "git+https://github.com/henrycmeen/dimo.git"
+        ])
+        print("Successfully updated DIMO!")
+    except subprocess.CalledProcessError as e:
+        print(f"Error updating DIMO: {e}", file=sys.stderr)
+        sys.exit(1)
 
 def main():
     parser = argparse.ArgumentParser(
         description="DIMO - Digital Archive Management Tools"
     )
     subparsers = parser.add_subparsers(dest='command', help='Available commands')
+    
+    # Add update command
+    update_parser = subparsers.add_parser('update',
+        help='Update DIMO to the latest version')
     
     # update-mets command
     update_parser = subparsers.add_parser('update-mets', 
@@ -62,7 +72,9 @@ def main():
     
     args = parser.parse_args()
     
-    if args.command == 'update-mets':
+    if args.command == 'update':
+        update_command(args)
+    elif args.command == 'update-mets':
         update_mets_command(args)
     elif args.command == 'test':
         test_command(args)
